8b3628d0da5ea4e14d9526c2e37d579b1dd18654
anasshaikh3812@gmail.com <Anas.Shaikh@einfochips.com>
2023-09-18 18:24:26 +0530

    Modified Client.cpp

--- Client.cpp
+++ Client.cpp
@@ -1,61 +1,52 @@
-// Client.cpp
 #include <iostream>
 #include <cstring>
+#include <cstdlib>
 #include <unistd.h>
+#include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
 
-const char *addr = "172.21.64.1";
+constexpr int PORT = 12345;
 
 int main() {
-    int clientSocket;
-    char buffer[1024];
-    struct sockaddr_in serverAddr;
-
-    clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (clientSocket < 0) {
-        perror("Error in socket");
-        exit(1);
-    }
-    std::cout << "Client socket created..." << std::endl;
-
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(2000);
-    serverAddr.sin_addr.s_addr = inet_addr(addr);
-
-    if (connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
-        perror("Error in connection");
-        exit(1);
-    }
-    std::cout << "Connected to server..." << std::endl;
-
-    // Main chat loop
-while (true) {
-    std::cout << "Client: ";
-    std::cin.getline(buffer, sizeof(buffer));
-    send(clientSocket, buffer, strlen(buffer), 0);
-
-    if (strcmp(buffer, "exit") == 0) {
-        std::cout << "Exiting..." << std::endl;
-        break;
+    int client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_socket == -1) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
     }
 
-    memset(buffer, 0, sizeof(buffer)); // Clear the buffer
-    ssize_t bytesReceived = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
-    if (bytesReceived < 0) {
-        perror("Error in receiving");
-        break;
-    } else if (bytesReceived == 0) {
-        std::cout << "Server disconnected." << std::endl;
-        break;
+    sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(PORT);
+    server_address.sin_addr.s_addr = inet_addr("172.25.4.13");
+
+    if (connect(client_socket, (struct sockaddr*)&server_address, sizeof(server_address)) == -1) {
+        perror("Connection failed");
+        exit(EXIT_FAILURE);
     }
 
-    buffer[bytesReceived] = '\0'; // Null-terminate the received message
-    std::cout << "Server: " << buffer << std::endl;
-}
+    char message[1024];
 
-    close(clientSocket);
+    while (true) {
+        std::cout << "Enter message (or 'q' to quit): ";
+        std::cin.getline(message, sizeof(message));
 
+        if (strcmp(message, "q") == 0) {
+            break;
+        }
+
+        send(client_socket, message, strlen(message), 0);
+
+        memset(message, 0, sizeof(message));
+        int bytes_received = recv(client_socket, message, sizeof(message), 0);
+        if (bytes_received <= 0) {
+            std::cerr << "Server disconnected." << std::endl;
+            break;
+        }
+
+        std::cout << "Server: " << message << std::endl;
+    }
+
+    close(client_socket);
     return 0;
 }
