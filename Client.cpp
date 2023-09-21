f64e103c3893223ac1304815e6bf4477a4ac883b
DJ7567 <divyaraj7567@gmail.com>
2023-09-21 18:27:27 +0530

    divyaraj-server-client

--- Client.cpp
+++ Client.cpp
@@ -4,12 +4,54 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <thread>
 #include <netinet/in.h>
 
-constexpr int PORT = 12345;
+constexpr int PORT = 11111;
+
+
+int client_socket;
+
+
+void handle_send(int a)
+{
+    char message[1024];
+
+    while(1)
+    {
+    std::cout << "Client hello ";
+    std::cin.getline(message, sizeof(message));
+
+    if (strcmp(message, "q") == 0) {
+          close(client_socket);
+    }
+
+    send(client_socket, message, strlen(message), 0);
+
+    memset(message, 0, sizeof(message));
+    }
+
+}
+
+void handle_rec(int b)
+{
+    char message[1024];
+
+    while(1)
+    {
+    int bytes_received = recv(client_socket, message, sizeof(message), 0);
+        if (bytes_received <= 0) {
+            std::cerr << "Server disconnected." << std::endl;
+             exit(1);
+        }
+
+    std::cout << "Server: " << message << std::endl;
+    }
+
+}
 
 int main() {
-    int client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    client_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (client_socket == -1) {
         perror("Socket creation failed");
         exit(EXIT_FAILURE);
@@ -18,35 +60,24 @@ int main() {
     sockaddr_in server_address;
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(PORT);
-    server_address.sin_addr.s_addr = inet_addr("172.25.4.13");
+    server_address.sin_addr.s_addr = INADDR_ANY;//inet_addr("172.25.4.13");
 
-    if (connect(client_socket, (struct sockaddr*)&server_address, sizeof(server_address)) == -1) {
+    if (connect(client_socket, (struct sockaddr*)&server_address, sizeof(server_address)) == -1) 
+    {
         perror("Connection failed");
         exit(EXIT_FAILURE);
     }
 
     char message[1024];
 
-    while (true) {
-        std::cout << "Enter message (or 'q' to quit): ";
-        std::cin.getline(message, sizeof(message));
-
-        if (strcmp(message, "q") == 0) {
-            break;
-        }
-
-        send(client_socket, message, strlen(message), 0);
-
-        memset(message, 0, sizeof(message));
-        int bytes_received = recv(client_socket, message, sizeof(message), 0);
-        if (bytes_received <= 0) {
-            std::cerr << "Server disconnected." << std::endl;
-            break;
-        }
-
-        std::cout << "Server: " << message << std::endl;
+    //while (true) 
+    {
+        
+        std::thread(handle_send, 1).detach();
+        std::thread(handle_rec, 1).detach();
+      
     }
-
-    close(client_socket);
+    while(1);
+    
     return 0;
 }
--- Server.cpp
+++ Server.cpp
@@ -8,28 +8,57 @@
 #include <thread>
 #include <vector>
 
-constexpr int PORT = 12345;
+using namespace std;
+
+constexpr int PORT = 11111;
 constexpr int MAX_CONNECTIONS = 5;
 
-std::vector<int> client_sockets;
+vector<int> client_sockets;
 
-void handle_client(int client_socket) {
-    char buffer[1024];
-    while (true) {
+
+void handle_rec(int a)
+{
+    while(1)
+    {
+
         memset(buffer, 0, sizeof(buffer));
         int bytes_received = recv(client_socket, buffer, sizeof(buffer), 0);
-        if (bytes_received <= 0) {
+        if (bytes_received <= 0) 
+        {
             close(client_socket);
             return;
         }
-        std::cout << "Received: " << buffer << std::endl;
 
-        // Broadcast the message to all clients
-        for (int socket : client_sockets) {
-            if (socket != client_socket) {
-                send(socket, buffer, bytes_received, 0);
-            }
+        cout << "client " << buffer << endl;
+    }
+}
+
+
+void handle_send(int a)
+{
+    cout << "server : ";
+        cin.getline(buffer, sizeof(buffer));
+
+        if (strcmp(buffer, "q") == 0) 
+        {
+           // break;
         }
+        
+        send(client_socket, buffer, strlen(buffer), 0);
+
+}
+
+void handle_client(int client_socket) {
+    char buffer[1024];
+    cout << "_________________________________________________" << client_socket << "\n";
+    cout << "Sucessfully connected to client " << client_socket << "\n";
+    cout << "_________________________________________________" << client_socket << "\n";
+    while (true) 
+    {
+        
+        thread(handle_send, 1).detach();
+        thread(handle_rec, 1).detach();
+
     }
 }
 
@@ -55,7 +84,7 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    std::cout << "Server listening on port " << PORT << std::endl;
+    cout << "Server listening on port " << PORT << endl;
 
     while (true) {
         sockaddr_in client_address;
@@ -68,9 +97,9 @@ int main() {
         }
 
         client_sockets.push_back(client_socket);
-
+        
         // Create a new thread to handle the client
-        std::thread(handle_client, client_socket).detach();
+        thread(handle_client, client_socket).detach();
     }
 
     close(server_socket);
