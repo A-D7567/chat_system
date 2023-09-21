69dce64fefd16561fe15047030ef1b33cd4e452e
anasshaikh3812@gmail.com <Anas.Shaikh@einfochips.com>
2023-09-21 18:36:11 +0530

    Modified Authentication Files

--- Client_Authentication.cpp
+++ Client_Authentication.cpp
@@ -1,58 +1,109 @@
-// Client.cpp
+// client.cpp
 #include <iostream>
 #include <cstring>
+#include <cstdlib>
 #include <unistd.h>
+#include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-int main() {
-    int clientSocket;
-    char buffer[1024];
-    struct sockaddr_in serverAddr;
+constexpr int PORT = 2222;
 
-    clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (clientSocket < 0) {
-        perror("Error in socket");
-        exit(1);
-    }
-    std::cout << "Client socket created..." << std::endl;
+using namespace std;
 
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(8080);
-    serverAddr.sin_addr.s_addr = INADDR_ANY;
-
-    if (connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
-        perror("Error in connection");
-        exit(1);
-    }
-    std::cout << "Connected to server..." << std::endl;
-
-    // Main chat loop
-while (true) {
-    std::cout << "Client: ";
-    std::cin.getline(buffer, sizeof(buffer));
-    send(clientSocket, buffer, strlen(buffer), 0);
-
-    if (strcmp(buffer, "exit") == 0) {
-        std::cout << "Exiting..." << std::endl;
-        break;
+int main()
+{
+    int client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_socket == -1)
+    {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
     }
 
-    memset(buffer, 0, sizeof(buffer)); // Clear the buffer
-    ssize_t bytesReceived = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
-    if (bytesReceived < 0) {
-        perror("Error in receiving");
-        break;
-    } else if (bytesReceived == 0) {
-        std::cout << "Server disconnected." << std::endl;
-        break;
+    sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(PORT);
+    server_address.sin_addr.s_addr = inet_addr("127.0.0.1"); // Change to the server's IP
+
+    if (connect(client_socket, (struct sockaddr *)&server_address, sizeof(server_address)) == -1)
+    {
+        perror("Connection failed");
+        exit(EXIT_FAILURE);
     }
 
-    buffer[bytesReceived] = '\0'; // Null-terminate the received message
-    std::cout << "Server: " << buffer << std::endl;
-}
+    char message[1024];
+    cout << "Select an option:" << endl;
+    cout << "1. Login" << endl;
+    cout << "2. Signup" << endl;
+    cout << "Enter your choice: ";
 
-    close(clientSocket);
+    int choice;
+    cin >> choice;
+    cin.ignore(); // Clear the newline character from the buffer
 
+    if (choice == 1)
+    {
+        // Login
+        send(client_socket, "login", strlen("login"), 0);
+
+        // Enter username
+        string username;
+        cout << "Enter username: ";
+        cin >> username;
+        send(client_socket, username.c_str(), username.size(), 0);
+
+        // Enter password
+        string password;
+        cout << "Enter password: ";
+        cin >> password;
+        send(client_socket, password.c_str(), password.size(), 0);
+
+        // Receive and handle server's response for login
+        memset(message, 0, sizeof(message));
+        int bytes_received = recv(client_socket, message, sizeof(message), 0);
+        if (bytes_received <= 0)
+        {
+            cout << "Server disconnected." << endl;
+        }
+        else
+        {
+            cout << "Server: " << message << endl;
+        }
+    }
+    else if (choice == 2)
+    {
+        // Signup
+        send(client_socket, "signup", strlen("signup"), 0);
+
+        // Enter a new username
+        string new_username;
+        cout << "Enter a new username: ";
+        cin >> new_username;
+        send(client_socket, new_username.c_str(), new_username.size(), 0);
+
+        // Enter a new password
+        string new_password;
+        cout << "Enter a new password: ";
+        cin >> new_password;
+        send(client_socket, new_password.c_str(), new_password.size(), 0);
+
+        // Receive and handle server's response for signup
+        memset(message, 0, sizeof(message));
+        int bytes_received = recv(client_socket, message, sizeof(message), 0);
+        if (bytes_received <= 0)
+        {
+            cout << "Server disconnected." << endl;
+        }
+        else
+        {
+            cout << "Server: " << message << endl;
+        }
+    }
+    else
+    {
+        cout << "Invalid choice. Please select 1 or 2." << endl;
+    }
+
+    close(client_socket);
     return 0;
 }
--- Server_Authentication.cpp
+++ Server_Authentication.cpp
@@ -1,72 +1,258 @@
-// Server.cpp
+// server.cpp
 #include <iostream>
 #include <cstring>
+#include <algorithm>
+#include <cstdlib>
 #include <unistd.h>
+#include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <map>
+#include <vector>
+#include <thread>
+#include <fstream>
+const int PORT = 2222;
+const int MAX_CONNECTIONS = 5;
+const int MAX_BUFFER_SIZE = 1024;
 
-int main() {
-    int serverSocket, newSocket;
-    char buffer[1024];
-    struct sockaddr_in serverAddr, newAddr;
-    socklen_t addr_size;
+using namespace std;
 
-    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (serverSocket < 0) {
-        perror("Error in socket");
-        exit(1);
+// user data structure
+struct UserData
+{
+    string username;
+    string password;
+};
+
+// User database using a map
+map<std::string, UserData> userDatabase;
+vector<UserData> user_data;
+vector<int> client_sockets;
+
+// Read file User.txt
+void loadUserFile()
+{
+    ifstream userFile("users.txt");
+    if (!userFile.is_open())
+    {
+        cout << "Error opening user file." << endl;
+        return;
     }
-    std::cout << "Server socket created..." << std::endl;
-
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(8080);
-    serverAddr.sin_addr.s_addr = inet_addr("10.126.65.66") 	;
-
-    if (bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
-        perror("Error in binding");
-        exit(1);
+    UserData user;
+    while (userFile >> user.username >> user.password)
+    {
+        user_data.push_back(user);
     }
-    std::cout << "Binding success..." << std::endl;
-
-    if (listen(serverSocket, 10) == 0) {
-        std::cout << "Listening..." << std::endl;
-    } else {
-        std::cout << "Error in listening" << std::endl;
-        exit(1);
-    }
-
-    addr_size = sizeof(newAddr);
-    newSocket = accept(serverSocket, (struct sockaddr*)&newAddr, &addr_size);
-    if (newSocket < 0) {
-        perror("Error in accepting");
-        exit(1);
-    }
-
-    // Main chat loop
-while (true) {
-    memset(buffer, 0, sizeof(buffer)); // Clear the buffer
-    ssize_t bytesReceived = recv(newSocket, buffer, sizeof(buffer) - 1, 0);
-    if (bytesReceived < 0) {
-        perror("Error in receiving");
-        break;
-    } else if (bytesReceived == 0) {
-        std::cout << "Client disconnected." << std::endl;
-        break;
-    }
-
-    buffer[bytesReceived] = '\0'; // Null-terminate the received message
-    std::cout << "Client: " << buffer << std::endl;
-
-    memset(buffer, 0, sizeof(buffer)); // Clear the buffer
-    std::cout << "Server: ";
-    std::cin.getline(buffer, sizeof(buffer));
-    send(newSocket, buffer, strlen(buffer), 0);
+    userFile.close();
 }
 
+// Write file user.txt
+void saveUserFile()
+{
+    ofstream userFile("users.txt");
+    if (!userFile.is_open())
+    {
+        cout << "Error opening user file." << endl;
+        return;
+    }
+    for (const UserData &user : user_data)
+    {
+        userFile << "User_Name : " << user.username << "  Password : " << user.password << endl;
+    }
+    userFile.close();
+}
 
+bool authenticateUser(const string &username, const string &password)
+{
+    for (const UserData &user : user_data)
+    {
+        if (user.username == username && user.password == password)
+        {
+            return true; // Authentication successful
+        }
+    }
+    return false; // Authentication failed
+}
 
-    close(newSocket);
-    close(serverSocket);
+bool isUsernameTaken(const string &username)
+{
+    for (const UserData &user : user_data)
+    {
+        if (user.username == username)
+        {
+            return true; // Username is already taken
+        }
+    }
+    return false; // Username is available
+}
 
+void handle_client(int client_socket)
+{
+    char buffer[MAX_BUFFER_SIZE];
+    bool authenticated = false;
+    string username;
+
+    while (true)
+    {
+        memset(buffer, 0, sizeof(buffer));
+        int bytes_received = recv(client_socket, buffer, sizeof(buffer), 0);
+        if (bytes_received <= 0)
+        {
+            close(client_socket);
+            return;
+        }
+
+        if (!authenticated)
+        {
+            string message(buffer);
+            if (message == "login")
+            {
+                send(client_socket, "Enter username: ", strlen("Enter username: "), 0);
+                memset(buffer, 0, sizeof(buffer));
+                int username_length = recv(client_socket, buffer, sizeof(buffer), 0);
+
+                if (username_length <= 0)
+                {
+                    cout << "Enter Valid Username.." << endl;
+                    close(client_socket);
+                    return;
+                }
+                string client_username(buffer, username_length);
+
+                send(client_socket, "Enter password: ", strlen("Enter password: "), 0);
+                memset(buffer, 0, sizeof(buffer));
+                int password_length = recv(client_socket, buffer, sizeof(buffer), 0);
+                if (password_length <= 0)
+                {
+                    cout << "Enter valid Password.." << endl;
+                    close(client_socket);
+                    return;
+                }
+                string client_password(buffer, password_length);
+
+                if (authenticateUser(client_username, client_password))
+                {
+                    authenticated = true;
+                    username = client_username;
+                    send(client_socket, "Authentication successful.", strlen("Authentication successful."), 0);
+                }
+                else
+                {
+                    send(client_socket, "Authentication failed. Try again.", strlen("Authentication failed. Try again."), 0);
+                }
+            }
+            else if (message == "signup")
+            {
+                send(client_socket, "Enter a new username: ", strlen("Enter a new username: "), 0);
+                memset(buffer, 0, sizeof(buffer));
+                int username_length = recv(client_socket, buffer, sizeof(buffer), 0);
+                if (username_length <= 0)
+                {
+                    cout << "Enter Valid Username.." << endl;
+                    close(client_socket);
+                    return;
+                }
+                string new_username(buffer, username_length);
+
+                send(client_socket, "Enter a new password: ", strlen("Enter a new password: "), 0);
+                memset(buffer, 0, sizeof(buffer));
+                int password_length = recv(client_socket, buffer, sizeof(buffer), 0);
+                if (password_length <= 0)
+                {
+                    cout << "Enter valid Password.." << endl;
+                    close(client_socket);
+                    return;
+                }
+                string new_password(buffer, password_length);
+                UserData new_user;
+                new_user.username = new_username;
+                new_user.password = new_password;
+                user_data.push_back(new_user);
+                saveUserFile();
+                send(client_socket, "Signup successful.", strlen("Signup successful."), 0);
+            }
+            else
+            {
+                send(client_socket, "Authentication required.", strlen("Authentication required."), 0);
+            }
+        }
+        else
+        {
+            // Chat function
+            while (true)
+            {
+                memset(buffer, 0, sizeof(buffer));
+                int bytes_received = recv(client_socket, buffer, sizeof(buffer), 0);
+                if (bytes_received <= 0)
+                {
+                    close(client_socket);
+                    client_sockets.erase(std::remove(client_sockets.begin(), client_sockets.end(), client_socket), client_sockets.end());
+                    return;
+                }
+
+                // Message to client
+                for (int socket : client_sockets)
+                {
+                    if (socket != client_socket)
+                    {
+                        send(socket, (username + ": " + std::string(buffer)).c_str(), bytes_received, 0);
+                    }
+                }
+            }
+        }
+    }
+}
+
+int main()
+{
+    vector<int> client_sockets;
+
+    loadUserFile();
+
+    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket == -1)
+    {
+        perror("Socket creation failed");
+        exit(0);
+    }
+
+    sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(PORT);
+    server_address.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(server_socket, (struct sockaddr *)&server_address, sizeof(server_address)) == -1)
+    {
+        perror("Binding failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_socket, MAX_CONNECTIONS) == -1)
+    {
+        perror("Listening failed");
+        exit(EXIT_FAILURE);
+    }
+
+    cout << "Server listening on port " << PORT << endl;
+
+    while (true)
+    {
+        sockaddr_in client_address;
+        socklen_t client_addr_len = sizeof(client_address);
+
+        int client_socket = accept(server_socket, (struct sockaddr *)&client_address, &client_addr_len);
+        if (client_socket == -1)
+        {
+            perror("Accepting connection failed");
+            continue;
+        }
+
+        client_sockets.push_back(client_socket);
+
+        // Thread to handle the client
+        std::thread(handle_client, client_socket).detach();
+    }
+    close(server_socket);
+    saveUserFile(); // Save user data
     return 0;
 }
